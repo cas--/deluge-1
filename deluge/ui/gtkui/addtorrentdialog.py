9cc2f19177f53dd742a04e2dd274f2881d0216ae
Calum Lind <calumlind+deluge@gmail.com>
2016-11-11 15:23:22 +0000

    [GTK3] Marked FIXME code changes for GTK3

--- deluge/ui/gtkui/addtorrentdialog.py
+++ deluge/ui/gtkui/addtorrentdialog.py
@@ -350,7 +350,9 @@ class AddTorrentDialog(component.Component):
                     split_files,
                 )
             self.add_files(None, split_files)
-        self.listview_files.expand_row(b'0', False)
+        # FIXME add back expand_row
+        # self.listview_files.expand_row(b'0', False)
+        self.listview_files.expand_all()
 
     def prepare_file(self, _file, file_name, file_num, download, files_storage):
         first_slash_index = file_name.find(os.path.sep)
@@ -990,7 +992,9 @@ class AddTorrentDialog(component.Component):
 
                 # We need to re-expand the view because it might contracted
                 # if we change the root iter
-                self.listview_files.expand_row(b'0', False)
+                # FIXME add back expand_row
+                # self.listview_files.expand_row('0', False)
+                self.listview_files.expand_all()
             else:
                 # This was a simple folder rename without any splits, so just
                 # change the path for itr
