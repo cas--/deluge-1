23872ec92c5904a7df3f924c998de62d0a5e4ade
Calum Lind <calumlind+deluge@gmail.com>
2018-09-20 11:45:12 +0100

    [Tests] Update tests for GTK3 new paths

--- deluge/tests/test_torrentview.py
+++ deluge/tests/test_torrentview.py
@@ -22,7 +22,7 @@ from .basetest import BaseTestCase
 
 # Allow running other tests without GTKUI dependencies available
 try:
-    from gobject import TYPE_UINT64
+    from gi.repository.GObject import TYPE_UINT64
 except ImportError as err:
     libs_available = False
     TYPE_UINT64 = 'Whatever'
@@ -30,11 +30,11 @@ except ImportError as err:
     traceback.print_exc()
 else:
     libs_available = True
-    from deluge.ui.gtkui.mainwindow import MainWindow  # pylint: disable=ungrouped-imports
-    from deluge.ui.gtkui.menubar import MenuBar
-    from deluge.ui.gtkui.torrentdetails import TorrentDetails
-    from deluge.ui.gtkui.torrentview import TorrentView
-    from deluge.ui.gtkui.gtkui import DEFAULT_PREFS
+    from deluge.ui.gtk3.mainwindow import MainWindow  # pylint: disable=ungrouped-imports
+    from deluge.ui.gtk3.menubar import MenuBar
+    from deluge.ui.gtk3.torrentdetails import TorrentDetails
+    from deluge.ui.gtk3.torrentview import TorrentView
+    from deluge.ui.gtk3.gtkui import DEFAULT_PREFS
 
 setup_translations()
 
