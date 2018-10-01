hugosenari/deluge

[GTK3] Change module structure from ui/gtkui to ui/gtk3
This moves the directory structure so that there is no conflict with the
old gtk2 UI. Also changes the conf and state files being loaded.

deluge/ui/gtk3/pluginmanager.py
@@ -1,0 +1,136 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2007, 2008 Andrew Resch <andrewresch@gmail.com>
+#
+# This file is part of Deluge and is licensed under GNU General Public License 3.0, or later, with
+# the additional special exception to link portions of this program with the OpenSSL library.
+# See LICENSE for more details.
+#
+
+from __future__ import unicode_literals
+
+import logging
+
+import deluge.component as component
+import deluge.pluginmanagerbase
+from deluge.configmanager import ConfigManager
+from deluge.ui.client import client
+
+log = logging.getLogger(__name__)
+
+
+class PluginManager(deluge.pluginmanagerbase.PluginManagerBase, component.Component):
+    def __init__(self):
+        component.Component.__init__(self, 'PluginManager')
+        self.config = ConfigManager('gtk3.conf')
+        deluge.pluginmanagerbase.PluginManagerBase.__init__(
+            self, 'gtk3.conf', 'deluge.plugin.gtk3',
+        )
+
+        self.hooks = {
+            'on_apply_prefs': [],
+            'on_show_prefs': [],
+        }
+
+        client.register_event_handler('PluginEnabledEvent', self._on_plugin_enabled_event)
+        client.register_event_handler('PluginDisabledEvent', self._on_plugin_disabled_event)
+
+    def register_hook(self, hook, function):
+        """Register a hook function with the plugin manager"""
+        try:
+            self.hooks[hook].append(function)
+        except KeyError:
+            log.warning('Plugin attempting to register invalid hook.')
+
+    def deregister_hook(self, hook, function):
+        """Deregisters a hook function"""
+        try:
+            self.hooks[hook].remove(function)
+        except KeyError:
+            log.warning('Unable to deregister hook %s', hook)
+
+    def start(self):
+        """Start the plugin manager"""
+        # Update the enabled_plugins from the core
+        client.core.get_enabled_plugins().addCallback(self._on_get_enabled_plugins)
+        for instance in self.plugins.values():
+            component.start([instance.plugin._component_name])
+
+    def stop(self):
+        # Disable the plugins
+        self.disable_plugins()
+
+    def update(self):
+        pass
+
+    def _on_get_enabled_plugins(self, enabled_plugins):
+        log.debug('Core has these plugins enabled: %s', enabled_plugins)
+        for plugin in enabled_plugins:
+            self.enable_plugin(plugin)
+
+    def _on_plugin_enabled_event(self, name):
+        try:
+            self.enable_plugin(name)
+        except Exception as ex:
+            log.warn('Failed to enable plugin "%s": ex: %s', name, ex)
+
+        self.run_on_show_prefs()
+
+    def _on_plugin_disabled_event(self, name):
+        self.disable_plugin(name)
+
+    # Hook functions
+    def run_on_show_prefs(self):
+        """This hook is run before the user is shown the preferences dialog.
+        It is designed so that plugins can update their preference page with
+        the config."""
+        log.debug('run_on_show_prefs')
+        for function in self.hooks['on_show_prefs']:
+            function()
+
+    def run_on_apply_prefs(self):
+        """This hook is run after the user clicks Apply or OK in the preferences
+        dialog.
+        """
+        log.debug('run_on_apply_prefs')
+        for function in self.hooks['on_apply_prefs']:
+            function()
+
+    # Plugin functions.. will likely move to own class..
+
+    def add_torrentview_text_column(self, *args, **kwargs):
+        return component.get('TorrentView').add_text_column(*args, **kwargs)
+
+    def remove_torrentview_column(self, *args):
+        return component.get('TorrentView').remove_column(*args)
+
+    def add_toolbar_separator(self):
+        return component.get('ToolBar').add_separator()
+
+    def add_toolbar_button(self, *args, **kwargs):
+        return component.get('ToolBar').add_toolbutton(*args, **kwargs)
+
+    def remove_toolbar_button(self, *args):
+        return component.get('ToolBar').remove(*args)
+
+    def add_torrentmenu_menu(self, *args):
+        return component.get('MenuBar').torrentmenu.append(*args)
+
+    def add_torrentmenu_separator(self):
+        return component.get('MenuBar').add_torrentmenu_separator()
+
+    def remove_torrentmenu_item(self, *args):
+        return component.get('MenuBar').torrentmenu.remove(*args)
+
+    def add_preferences_page(self, *args):
+        return component.get('Preferences').add_page(*args)
+
+    def remove_preferences_page(self, *args):
+        return component.get('Preferences').remove_page(*args)
+
+    def update_torrent_view(self, *args):
+        return component.get('TorrentView').update(*args)
+
+    def get_selected_torrents(self):
+        """Returns a list of the selected torrent_ids"""
+        return component.get('TorrentView').get_selected_torrents()

deluge/ui/gtk3/removetorrentdialog.py
@@ -1,0 +1,86 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2007-2008 Andrew Resch <andrewresch@gmail.com>
+#
+# This file is part of Deluge and is licensed under GNU General Public License 3.0, or later, with
+# the additional special exception to link portions of this program with the OpenSSL library.
+# See LICENSE for more details.
+#
+
+from __future__ import unicode_literals
+
+import logging
+import os
+
+from gi.repository import Gtk
+
+import deluge.common
+import deluge.component as component
+from deluge.ui.client import client
+
+log = logging.getLogger(__name__)
+
+
+class RemoveTorrentDialog(object):
+    """
+    This class is used to create and show a Remove Torrent Dialog.
+
+    :param torrent_ids: the torrent_ids to remove
+    :type torrent_ids: list of torrent_ids
+
+    :raises TypeError: if `torrent_id` is not a sequence type
+    :raises ValueError: if `torrent_id` contains no torrent_ids or is None
+
+    """
+    def __init__(self, torrent_ids, delete_files=False):
+        if not isinstance(torrent_ids, list) and not isinstance(torrent_ids, tuple):
+            raise TypeError('requires a list of torrent_ids')
+
+        if not torrent_ids:
+            raise ValueError('requires a list of torrent_ids')
+
+        self.__torrent_ids = torrent_ids
+
+        self.builder = Gtk.Builder()
+        self.builder.add_from_file(deluge.common.resource_filename(
+            __package__, os.path.join('glade', 'remove_torrent_dialog.ui'),
+        ))
+
+        self.__dialog = self.builder.get_object('remove_torrent_dialog')
+        self.__dialog.set_transient_for(component.get('MainWindow').window)
+
+        self.builder.connect_signals(self)
+        self.builder.get_object('delete_files').set_active(delete_files)
+        label_title = self.builder.get_object('label_title')
+        label_torrents = self.builder.get_object('label_torrents')
+        num_torrents = len(self.__torrent_ids)
+        if num_torrents == 1:
+            label_torrents.set_markup(component.get('TorrentView').get_torrent_status(self.__torrent_ids[0])['name'])
+        else:
+            label_title.set_markup(_('Remove the selected torrents?'))
+            label_torrents.set_markup(_('Total of %s torrents selected') % num_torrents)
+
+    def on_delete_files_toggled(self, widget):
+        self.builder.get_object('warning_label').set_visible(widget.get_active())
+
+    def __remove_torrents(self, remove_data):
+        # Unselect all to avoid issues with the selection changed event
+        component.get('TorrentView').treeview.get_selection().unselect_all()
+
+        def on_removed_finished(errors):
+            if errors:
+                log.info('Error(s) occured when trying to delete torrent(s).')
+                for t_id, e_msg in errors:
+                    log.warn('Error removing torrent %s : %s', t_id, e_msg)
+
+        d = client.core.remove_torrents(self.__torrent_ids, remove_data)
+        d.addCallback(on_removed_finished)
+
+    def run(self):
+        """
+        Shows the dialog and awaits for user input.  The user can select to
+        remove the torrent(s) from the session with or without their data.
+        """
+        if self.__dialog.run() == Gtk.ResponseType.OK:
+            self.__remove_torrents(self.builder.get_object('delete_files').get_active())
+        self.__dialog.destroy()

cas--/deluge-1

deluge/ui/gtk3/pluginmanager.py
@@ -1,0 +1,136 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2007, 2008 Andrew Resch <andrewresch@gmail.com>
+#
+# This file is part of Deluge and is licensed under GNU General Public License 3.0, or later, with
+# the additional special exception to link portions of this program with the OpenSSL library.
+# See LICENSE for more details.
+#
+
+from __future__ import unicode_literals
+
+import logging
+
+import deluge.component as component
+import deluge.pluginmanagerbase
+from deluge.configmanager import ConfigManager
+from deluge.ui.client import client
+
+log = logging.getLogger(__name__)
+
+
+class PluginManager(deluge.pluginmanagerbase.PluginManagerBase, component.Component):
+    def __init__(self):
+        component.Component.__init__(self, 'PluginManager')
+        self.config = ConfigManager('gtk3.conf')
+        deluge.pluginmanagerbase.PluginManagerBase.__init__(
+            self, 'gtk3.conf', 'deluge.plugin.gtk3',
+        )
+
+        self.hooks = {
+            'on_apply_prefs': [],
+            'on_show_prefs': [],
+        }
+
+        client.register_event_handler('PluginEnabledEvent', self._on_plugin_enabled_event)
+        client.register_event_handler('PluginDisabledEvent', self._on_plugin_disabled_event)
+
+    def register_hook(self, hook, function):
+        """Register a hook function with the plugin manager"""
+        try:
+            self.hooks[hook].append(function)
+        except KeyError:
+            log.warning('Plugin attempting to register invalid hook.')
+
+    def deregister_hook(self, hook, function):
+        """Deregisters a hook function"""
+        try:
+            self.hooks[hook].remove(function)
+        except KeyError:
+            log.warning('Unable to deregister hook %s', hook)
+
+    def start(self):
+        """Start the plugin manager"""
+        # Update the enabled_plugins from the core
+        client.core.get_enabled_plugins().addCallback(self._on_get_enabled_plugins)
+        for instance in self.plugins.values():
+            component.start([instance.plugin._component_name])
+
+    def stop(self):
+        # Disable the plugins
+        self.disable_plugins()
+
+    def update(self):
+        pass
+
+    def _on_get_enabled_plugins(self, enabled_plugins):
+        log.debug('Core has these plugins enabled: %s', enabled_plugins)
+        for plugin in enabled_plugins:
+            self.enable_plugin(plugin)
+
+    def _on_plugin_enabled_event(self, name):
+        try:
+            self.enable_plugin(name)
+        except Exception as ex:
+            log.warning('Failed to enable plugin "%s": ex: %s', name, ex)
+
+        self.run_on_show_prefs()
+
+    def _on_plugin_disabled_event(self, name):
+        self.disable_plugin(name)
+
+    # Hook functions
+    def run_on_show_prefs(self):
+        """This hook is run before the user is shown the preferences dialog.
+        It is designed so that plugins can update their preference page with
+        the config."""
+        log.debug('run_on_show_prefs')
+        for function in self.hooks['on_show_prefs']:
+            function()
+
+    def run_on_apply_prefs(self):
+        """This hook is run after the user clicks Apply or OK in the preferences
+        dialog.
+        """
+        log.debug('run_on_apply_prefs')
+        for function in self.hooks['on_apply_prefs']:
+            function()
+
+    # Plugin functions.. will likely move to own class..
+
+    def add_torrentview_text_column(self, *args, **kwargs):
+        return component.get('TorrentView').add_text_column(*args, **kwargs)
+
+    def remove_torrentview_column(self, *args):
+        return component.get('TorrentView').remove_column(*args)
+
+    def add_toolbar_separator(self):
+        return component.get('ToolBar').add_separator()
+
+    def add_toolbar_button(self, *args, **kwargs):
+        return component.get('ToolBar').add_toolbutton(*args, **kwargs)
+
+    def remove_toolbar_button(self, *args):
+        return component.get('ToolBar').remove(*args)
+
+    def add_torrentmenu_menu(self, *args):
+        return component.get('MenuBar').torrentmenu.append(*args)
+
+    def add_torrentmenu_separator(self):
+        return component.get('MenuBar').add_torrentmenu_separator()
+
+    def remove_torrentmenu_item(self, *args):
+        return component.get('MenuBar').torrentmenu.remove(*args)
+
+    def add_preferences_page(self, *args):
+        return component.get('Preferences').add_page(*args)
+
+    def remove_preferences_page(self, *args):
+        return component.get('Preferences').remove_page(*args)
+
+    def update_torrent_view(self, *args):
+        return component.get('TorrentView').update(*args)
+
+    def get_selected_torrents(self):
+        """Returns a list of the selected torrent_ids"""
+        return component.get('TorrentView').get_selected_torrents()

deluge/ui/gtk3/removetorrentdialog.py
@@ -1,0 +1,86 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2007-2008 Andrew Resch <andrewresch@gmail.com>
+#
+# This file is part of Deluge and is licensed under GNU General Public License 3.0, or later, with
+# the additional special exception to link portions of this program with the OpenSSL library.
+# See LICENSE for more details.
+#
+
+from __future__ import unicode_literals
+
+import logging
+import os
+
+from gi.repository import Gtk
+
+import deluge.common
+import deluge.component as component
+from deluge.ui.client import client
+
+log = logging.getLogger(__name__)
+
+
+class RemoveTorrentDialog(object):
+    """
+    This class is used to create and show a Remove Torrent Dialog.
+
+    :param torrent_ids: the torrent_ids to remove
+    :type torrent_ids: list of torrent_ids
+
+    :raises TypeError: if `torrent_id` is not a sequence type
+    :raises ValueError: if `torrent_id` contains no torrent_ids or is None
+
+    """
+    def __init__(self, torrent_ids, delete_files=False):
+        if not isinstance(torrent_ids, list) and not isinstance(torrent_ids, tuple):
+            raise TypeError('requires a list of torrent_ids')
+
+        if not torrent_ids:
+            raise ValueError('requires a list of torrent_ids')
+
+        self.__torrent_ids = torrent_ids
+
+        self.builder = Gtk.Builder()
+        self.builder.add_from_file(deluge.common.resource_filename(
+            __package__, os.path.join('glade', 'remove_torrent_dialog.ui'),
+        ))
+
+        self.__dialog = self.builder.get_object('remove_torrent_dialog')
+        self.__dialog.set_transient_for(component.get('MainWindow').window)
+
+        self.builder.connect_signals(self)
+        self.builder.get_object('delete_files').set_active(delete_files)
+        label_title = self.builder.get_object('label_title')
+        label_torrents = self.builder.get_object('label_torrents')
+        num_torrents = len(self.__torrent_ids)
+        if num_torrents == 1:
+            label_torrents.set_markup(component.get('TorrentView').get_torrent_status(self.__torrent_ids[0])['name'])
+        else:
+            label_title.set_markup(_('Remove the selected torrents?'))
+            label_torrents.set_markup(_('Total of %s torrents selected') % num_torrents)
+
+    def on_delete_files_toggled(self, widget):
+        self.builder.get_object('warning_label').set_visible(widget.get_active())
+
+    def __remove_torrents(self, remove_data):
+        # Unselect all to avoid issues with the selection changed event
+        component.get('TorrentView').treeview.get_selection().unselect_all()
+
+        def on_removed_finished(errors):
+            if errors:
+                log.info('Error(s) occured when trying to delete torrent(s).')
+                for t_id, e_msg in errors:
+                    log.warning('Error removing torrent %s : %s', t_id, e_msg)
+
+        d = client.core.remove_torrents(self.__torrent_ids, remove_data)
+        d.addCallback(on_removed_finished)
+
+    def run(self):
+        """
+        Shows the dialog and awaits for user input.  The user can select to
+        remove the torrent(s) from the session with or without their data.
+        """
+        if self.__dialog.run() == Gtk.ResponseType.OK:
+            self.__remove_torrents(self.builder.get_object('delete_files').get_active())
+        self.__dialog.destroy()